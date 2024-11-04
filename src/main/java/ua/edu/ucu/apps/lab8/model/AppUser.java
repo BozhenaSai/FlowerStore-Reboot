1f7fd02bcb6c4c523ac8a7594ea37b1fc27e19af
BozhenaSai <149332573+BozhenaSai@users.noreply.github.com>
2024-11-04 21:10:02 +0200

    implemented

---
+++ src/main/java/ua/edu/ucu/apps/lab8/model/AppUser.java
@@ -0,0 +1,26 @@
+package ua.edu.ucu.apps.lab8.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Data
+@Table
+@Entity(name = "app_user")
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String email;
+    private LocalDate dob;
+    @Transient
+    private Integer age;
+
+    public int getAge() {
+        return Period.between(dob, LocalDate.now()).getYears();
+    }
+}
